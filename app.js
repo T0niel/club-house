1495617adb9a71dae14e4e381e6a07dd221fbc2b
Elton Rexha <elton.rexha123@outlook.com>
2024-11-20 15:58:42 +0100

    configure tailwind, and serve public

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const indexRouter = require('./routes/index');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-})
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000; 
 
