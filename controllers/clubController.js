e062de8e8529912cfbfd1b31d272958cd96595fc
Elton Rexha <elton.rexha123@outlook.com>
2024-11-22 21:05:07 +0100

    make club name unique and implement create club

--- controllers/clubController.js
+++ controllers/clubController.js
@@ -1,7 +1,64 @@
-function getCreateClub(req, res){
-    res.render('homepage', {user: req.user, showModal: true})
-}
+const { body, validationResult } = require('express-validator');
+const { insertClub } = require('../db/queries/clubQueries');
+const bcrypt = require('bcryptjs');
+const util = require('util');
+const hashAsync = util.promisify(bcrypt.hash);
+
+const getCreateClub = (req, res) => {
+  res.render('homepage', { user: req.user, showModal: true });
+};
+
+const createClubSchema = [
+  body('name')
+    .notEmpty()
+    .withMessage('Club name is required')
+    .isLength({ min: 3, max: 10 })
+    .withMessage('Club name must be between 3 at 10 characters'),
+  body('description')
+    .optional({values: 'falsy'})
+    .trim()
+    .isLength({ min: 4, max: 40 })
+    .withMessage('Club description should be between 4 and 20 characters'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password not provided')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.confirm_password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
+  body('confirm_password')
+    .notEmpty()
+    .withMessage('Confirm password not provided'),
+];
+
+const createClub = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.locals.errors = errors.array();
+    res.render('homepage', {
+      user: req.user,
+      showModal: true,
+    });
+    return;
+  }
+
+  const { password, name, description } = req.body;
+  const { id } = req.user;
+  try {
+    const passwordHash = await hashAsync(password, 10);
+    await insertClub(name, description, id, passwordHash);
+  } catch (e) {
+    next(e);
+    return;
+  }
+  res.redirect('/');
+};
 
 module.exports = {
-    getCreateClub
-}
+  getCreateClub,
+  createClub: [createClubSchema, createClub],
+};
