fb7ba4b6a30fa3430ea058b867ea8b84bbe92671
Elton <elton.rexha123@outlook.com>
2024-11-25 16:54:04 +0100

    Posts (#8)
    * create post view, when creating club join it
    
    * Display posts, create posts, and more
    
    Created the posts view and styled it, displayed the posts on it,
    and created the logic needed for creating posts from a form
    
    * refactor postController
    
    * Add delete posts, changed the database
    
    Added option to delete posts for the users,
    changed the database to hold the status of a post
    which can be deleted or active, also added user
    status which we are going to need later
    
    * Fix post bug
    
    Instead of querying all the posts of all clubs we query only
    the posts that belong to that club

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,10 +1,20 @@
 const { Router } = require('express');
-const { getPostsPage, getCreatePostPage, createPost } = require('../controllers/postController');
+const {
+  getPostsPage,
+  getCreatePostPage,
+  createPost,
+  deletePost,
+} = require('../controllers/postController');
 const isAuth = require('../middlewares/isAuth');
 const router = new Router();
 
 router.get('/:club', isAuth, getPostsPage);
 router.get('/:club/create', isAuth, getCreatePostPage);
 router.post('/:club/create', isAuth, createPost);
+
+router.get('/:club/create', isAuth, getCreatePostPage);
+router.post('/:club/create', isAuth, createPost);
+
+router.post('/:club/delete', isAuth, deletePost);
 
 module.exports = router;
